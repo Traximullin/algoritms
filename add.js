e6caf6e3be5a53ce68c5ad5274c7832643ba64f1
rtr <rtr@vt2b.ru>
2023-04-25 16:57:29 +0500

    add

--- add.js
+++ add.js
@@ -1,19 +1,5 @@
-const add = (n) => {
-    let result = n
-    console.log(result)
-    return function sum(b) {
-        console.log(b)
-        if(b) {
-            result += b
-            return (c) => {
-                if(c) {
-                    return sum(c)
-                }
-                return result
-            }
-        }
-        return result
-    }
+function add(n){
+  const func = (b) => add(n + b)
+  func.valueOf = () => n
+  return func
 }
-const test = add(1)(2)(3)(4)(5)(6)(7)
-console.log(test)
