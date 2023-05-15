9aff733aca6fda0e34237b533287cf00e61b7601
rtr <rtr@vt2b.ru>
2023-05-15 13:25:47 +0500

    counter

---
+++ createCounter.js
@@ -0,0 +1,14 @@
+{
+    const createCounter = function(n) {
+        let counter = n
+
+        return function() {
+            return counter++
+        };
+    };
+
+    const counter = createCounter(10)
+    console.log(counter())
+    console.log(counter())
+    console.log(counter())
+}
