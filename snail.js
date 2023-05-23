d49cb2a1f27e92d51fa7c0374b163f2d9884bfb5
rtr <rtr@vt2b.ru>
2023-05-23 13:39:40 +0500

    snail

--- snail.js
+++ snail.js
@@ -2,13 +2,33 @@
     Array.prototype.snail = function(rowsCount, colsCount) {
         const result = []
 
-        for(let i = 0; result.length !== rowsCount; i+= rowsCount) {
-            result.push(this.splice(0,colsCount))
+        if(rowsCount * colsCount !== this.length) {
+            return result
+        }
+
+        for(let i = 0; i < colsCount; i++) {
+            if(i % 2) {
+                this.push(this.splice(0,rowsCount).reverse())
+                continue
+            }
+            this.push(this.splice(0,rowsCount))
+        }
+
+        for(let i = 0; i < rowsCount; i++) {
+
+            result.push([])
+            
+            for(let j = 0; j < colsCount; j++){
+                result[i].push(this[j][i])
+            }
+
         }
 
         return result
     }
 
-    const arr = [19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15]
-    console.log(arr.snail(5,4))
+    const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+
+    console.log(arr.snail(6,3))
 }
