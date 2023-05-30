5b0d6f6cc5c59468a25a1619c7aeec37385337d4
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-05-30 21:44:42 +0300

    runningum

--- removeElement.ts
+++ removeElement.ts
@@ -3,8 +3,10 @@
         // return nums.filter(num => num !== val)
     
         while(nums.indexOf(val) !== -1) {
-            
+            nums.splice(nums.indexOf(val),1)
         }
+
+        return nums
     };
 
     console.log(removeElement([3,2,2,3],3))
