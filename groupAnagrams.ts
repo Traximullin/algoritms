4ae4ab8f80f3d0c23649a17c5566ee25c8859453
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-06-03 13:32:45 +0300

    groupAnagrams

--- groupAnagrams.ts
+++ groupAnagrams.ts
@@ -1,9 +1,30 @@
 {
+    const sortedWord = (str) => {
+        return str.toLowerCase().split("").sort().join("")
+    }
+
     const groupAnagrams = function(strs) {
-        const result: string[] = []       
-        const new_array = strs.map(elem => elem.toLowerCase().split("").sort().join("")).sort()
-        console.log(new_array)
-        console.log(strs)
+        // const map = {}
+ 
+        // for(const word of strs) {
+        //     const sorted = sortedWord(word)
+        //     if(!map[sorted]) map[sorted] = []
+        //     map[sorted].push(word)
+        // }
+
+        // return Object.values(map)
+
+        const map = new Map()
+        for(const word of strs) {
+            const sorted = sortedWord(word)
+            if(map.has(sorted)) {
+                map.get(sorted).push(word)
+            } else {
+                map.set(sorted, [word])
+            }
+        }
+
+        return Array.from(map.values())
     }
 
     console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
