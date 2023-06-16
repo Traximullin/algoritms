a08ecdeb72151f461086dc23f9753f7159d2dc76
rtr <rtr@vt2b.ru>
2023-06-16 17:26:32 +0500

    Calculator

--- isSameTree.ts
+++ isSameTree.ts
@@ -1,6 +1,6 @@
 {
     function TreeNode(val, left, right) {
-        this.val = (val===undefined ? 0 : val)
+        this.val = (val===undefined ? 0 : val) 
         this.left = (left===undefined ? null : left)
         this.right = (right===undefined ? null : right)
     }
