3a14a00f0c8dabdab5f20a0264e32fda408b684a
tr.rahimullin <tr.rahimullin@corp.tensor.ru>
2023-06-26 21:12:42 +0300

    longestCommonPrefix

---
+++ longestCommonPrefix.ts
@@ -0,0 +1,15 @@
+{
+    // !TODO
+    const longestCommonPrefix = function(strs) {
+        let result = ""
+        const firstWord = strs[0]
+        console.log(firstWord)
+
+        for(let word of strs) {
+
+        }
+        return result
+    };
+
+    console.log(longestCommonPrefix(["flower","flow","flight"]))
+}
