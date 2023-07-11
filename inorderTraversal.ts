b3f96fd69ed9ab560c81160334282ab77830bf3c
traximullin <traximullin@bk.ru>
2023-07-11 21:33:49 +0300

    inorderTraversal

---
+++ inorderTraversal.ts
@@ -0,0 +1,26 @@
+{
+    /*
+        !TODO 
+        https://leetcode.com/problems/binary-tree-inorder-traversal/
+    */
+
+    /**
+     * Definition for a binary tree node.
+     * function TreeNode(val, left, right) {
+     *     this.val = (val===undefined ? 0 : val)
+     *     this.left = (left===undefined ? null : left)
+     *     this.right = (right===undefined ? null : right)
+     * }
+     */
+
+    const inorderTraversal = function(root) {
+        const result = []
+
+        const test = () => {
+
+        }
+
+        return result
+    };
+
+}
