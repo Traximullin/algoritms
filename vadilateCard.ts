93ea2f3c0b2c812ea96c9df9d7a2e3dca417658a
traximullin <traximullin@bk.ru>
2023-07-13 21:46:50 +0300

    luna-algoritms-fix

--- vadilateCard.ts
+++ vadilateCard.ts
@@ -1,7 +1,15 @@
 {
+    // function validate(n) {
+    //     n = Array.from(String(n), Number).reverse();
+    //     return n.reduce((sum, digit, index) => {
+    //       if (index & 1) digit <<= 1;
+    //       if (digit > 9) digit -= 9;
+    //       return sum + digit;
+    //     }, 0) % 10 == 0;
+    // }
+
     const helper = (num: number) => {
         const result = num * 2
-
         if(result > 9) {
             return Array.from(String(result), Number).reduce((prev,curr) => prev + curr,0)
         }
@@ -11,12 +19,13 @@
 
     function validate(n){
         n = Array.from(String(n), Number)
+            .reverse()
             .map(
                 (item, index) => index % 2 ? helper(item) : item
             )
             .reduce((prev,curr) => prev + curr,0)
 
-        return n % 10 === 0
+            return n % 10 === 0
     }
 
     console.log(validate(1230))
