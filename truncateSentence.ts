b0a0c0d591d66f8d7e4db76f8b5a196e3d9026db
traximullin <traximullin@bk.ru>
2023-07-29 18:31:43 +0300

    https://leetcode.com/problems/truncate-sentence/description/

---
+++ truncateSentence.ts
@@ -0,0 +1,7 @@
+{
+    const truncateSentence = function(s, k) {
+        return s.split(" ").splice(0,k).join(" ")
+    };
+
+    console.log(truncateSentence("Hello how are you Contestant", 4))
+}
