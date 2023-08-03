ce13872d1823cf2f4e20b5a85b210019c8de34de
traximullin <traximullin@bk.ru>
2023-08-03 21:34:00 +0300

    https://leetcode.com/problems/find-first-palindromic-string-in-the-array refactor

--- firstPalindrome.ts
+++ firstPalindrome.ts
@@ -1,13 +1,25 @@
 {
 
-    const helper = (word: string) => {
-        return word.split("").reverse().join("")
+    const isPalindrome  = (word: string) => {
+        let start  = 0,
+            end  = word.length - 1
+
+        while(start < end) {
+            if(word[start] !== word[end]) {
+                return false
+            }
+
+            start++
+            end--
+        }
+
+        return true
     }
 
     const firstPalindrome = function(words) {
     
         for(let word of words) {
-            if(word === helper(word)) {
+            if(isPalindrome(word)) {
                 return word
             }
         }
@@ -16,4 +28,5 @@
     };
 
     console.log(firstPalindrome(["abc","car","ada","racecar","cool"]))
+    console.log(isPalindrome("ada"))
 }
