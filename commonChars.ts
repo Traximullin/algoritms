5bcb9f019972906ddcf6bbbe08fda0650e6c8b23
traximullin <traximullin@bk.ru>
2023-08-26 15:30:30 +0300

    https://leetcode.com/problems/find-common-characters

--- commonChars.ts
+++ commonChars.ts
@@ -1,22 +1,38 @@
 {
-    // !TODO 
-    // https://leetcode.com/problems/find-common-characters/description/
     const commonChars = function(words) {
-        const result: string[] = []
+        
+        let res = [...words[0]]
 
-        for(let i = 0; i < words[0].length; i++) {
-            let flag = true;
+        for(let i = 1;i<words.length;i++){
 
-            for(let j = 1; j < words.length; j++) {
-                if (!words[j].includes(words[0][i])) {
-                    flag = false
-                } 
-            }
-            if (flag)
-                result.push(words[0][i])
+            res = res.filter(char =>{
+                const l = words[i].length
+
+                words[i] = words[i].replace(char, "");
+                
+                return l > words[i].length
+            })
+
         }
 
-        return Array.from(new Set(result))
+        return res;
+        // const result: string[] = []
+
+        // for(let i = 0; i < words[0].length; i++) {
+        //     let flag = true;
+
+        //     for(let j = 1; j < words.length; j++) {
+        //         if (!words[j].includes(words[0][i])) {
+        //             flag = false
+        //         } else {
+        //             words[j] = words[j].replace(words[0][i],"")
+        //         }
+        //     }
+        //     if (flag)
+        //         result.push(words[0][i])
+        // }
+
+        // return result
     };
 
     console.log(commonChars(["cool","lock","cook"])
