127d131b1f9420878bd7c6792e1c018cd55a9c62
traximullin <traximullin@bk.ru>
2023-08-30 21:28:09 +0300

    https://leetcode.com/problems/pascals-triangle

--- pascals-triangle.ts
+++ pascals-triangle.ts
@@ -4,14 +4,14 @@
     const generate = function(numRows) {
         const result: number[][] = []
     
-        for(let i = 1; i <= numRows; i++) {
-            const node: number[] = []
-
-            for(let j = 0; j < i; j++) {
-                node.push(1)
+        for (let i = 0; i < numRows; i++) {
+            result[i] = [1]
+            
+            for (let j = 1; j < i; j++) {
+                result[i][j] = result[i-1][j-1] + result[i-1][j]
             }
-
-            result.push(node)
+            
+            result[i][i] = 1;
         }
 
         return result
