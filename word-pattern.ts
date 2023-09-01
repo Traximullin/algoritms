f2b71e2b30f913d7acd95a3bc8279028a1545579
traximullin <traximullin@bk.ru>
2023-09-01 09:26:36 +0300

    https://leetcode.com/problems/word-pattern

--- word-pattern.ts
+++ word-pattern.ts
@@ -1,18 +1,20 @@
 {
-    // !TODO
-    // https://leetcode.com/problems/word-pattern/?envType=study-plan-v2&envId=top-interview-150
     const wordPattern = function(pattern, s) {
         const map = new Map()
+        const patternMap = new Map()
 
-        for(const [index, word] of s.split(" ").entries()) {
-            if(map.has(pattern[index])) {
-                console.log('!')
-                if(map.get(pattern[index]) !== word) {
-                    return false
-                }
-                
-            } else {
-                map.set(pattern[index], word)
+        const splitS = s.split(" ");
+
+	    if (pattern.length !== splitS.length) return false;
+
+        for (let i = 0; i < pattern.length; i++) {
+            if (!patternMap.has(pattern[i])) patternMap.set(pattern[i], i);
+            if (!map.has(splitS[i])) map.set(splitS[i], i);
+        }
+
+        for (let i = 0; i < pattern.length; i++) {
+            if (map.get(splitS[i]) !== patternMap.get(pattern[i])) {
+                return false;
             }
         }
 
