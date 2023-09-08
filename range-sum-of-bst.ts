1c508e14ac3e03c8aeff185fa56bc25adbf37098
traximullin <traximullin@bk.ru>
2023-09-08 23:03:54 +0300

    https://leetcode.com/problems/range-sum-of-bst/submissions/

---
+++ range-sum-of-bst.ts
@@ -0,0 +1,15 @@
+{
+    const rangeSumBST = (root, low, high) => {
+        if (!root) return 0
+        let sum = 0
+    
+        if (low <= root.val && root.val <= high) sum += root.val
+    
+        if (root.left) sum += rangeSumBST(root.left, low, high)
+        if (root.right) sum += rangeSumBST(root.right, low, high)
+    
+    
+        return sum
+        
+    };
+}
