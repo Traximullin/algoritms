6ce35430478dc0bed4239f75baa4c7b23cc0c213
traximullin <traximullin@bk.ru>
2023-10-01 21:34:26 +0300

    https://leetcode.com/problems/a-number-after-a-double-reversal

---
+++ a-number-after-a-double-reversal.ts
@@ -0,0 +1,13 @@
+{
+    const isSameAfterReversals = function(num) {
+        if(num === 0) 
+            return true
+        
+        if(num % 10 === 0)
+            return false
+        
+        return true
+    };
+
+    console.log(isSameAfterReversals(0))
+}
