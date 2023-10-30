653f84631f2d9e701f8b755adaefb9b98613f39d
traximullin <traximullin@bk.ru>
2023-10-30 16:01:37 +0300

    https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits

---
+++ sort-integers-by-the-number-of-1-bits.ts
@@ -0,0 +1,13 @@
+{
+    var sortByBits = function (arr) {
+        let result = []
+        arr.sort((a, b) => a - b)
+        
+        for (let i = 0; i < arr.length; i++) {
+            let binary = arr[i].toString(2).split('').filter(b => b == 1).length
+            result.push([binary, arr[i]])
+        }
+        result.sort((a, b) => a[0] - b[0])
+        return result.map(arr => arr[1])
+    };
+}
