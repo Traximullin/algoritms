0000186f60c1edddf76dc1caf575a86a1ffc07e6
traximullin <traximullin@bk.ru>
2023-11-05 16:47:43 +0300

    https://leetcode.com/problems/maximum-depth-of-n-ary-tree

---
+++ maximum-depth-of-n-ary-tree.ts
@@ -0,0 +1,14 @@
+{
+    const maxDepth = function(root) {
+        if(!root) 
+            return 0
+        
+        let max = 0
+    
+        for(let child of root.children) {
+           max = Math.max(max, maxDepth(child))
+        }
+
+        return max + 1
+    }
+}
