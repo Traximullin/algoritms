b9c847adb2e8fc79568e7211d1723cd486e1057f
traximullin <traximullin@bk.ru>
2023-11-17 15:36:24 +0300

    https://leetcode.com/problems/lexicographically-smallest-palindrome

---
+++ lexicographically-smallest-palindrome.ts
@@ -0,0 +1,15 @@
+{
+    const makeSmallestPalindrome = function(s) {
+        let str = s.split('')
+
+        for(let i = s.length - 1, j = 0; i >= Math.ceil(s.length / 2); i--, j++) {
+            if(str[i] < str[j]) {
+                str[j] = str[i]
+            } else {
+                str[i] = str[j]
+            }
+        }
+        
+        return str.join('')
+    };
+}
