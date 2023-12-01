47d1f471d718703de65ef3eb90bcc1340c16fc27
traximullin <traximullin@bk.ru>
2023-12-01 21:17:27 +0300

    https://leetcode.com/problems/remove-outermost-parentheses

---
+++ remove-outermost-parentheses.ts
@@ -0,0 +1,17 @@
+{
+    const removeOuterParentheses = function (s) {
+        let openCount = 0;
+        let output = "";
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] === "(") {
+                if (openCount) output += s[i];
+                openCount++;
+            }
+            else if (s[i] === ")") {
+                openCount--;
+                if (openCount) output += s[i];
+            }
+        }
+        return output;
+    };
+}
