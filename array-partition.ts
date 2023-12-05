704f0107eb48f3bfe834b03d1bb3ee598f941f03
traximullin <traximullin@bk.ru>
2023-12-05 20:53:14 +0300

    https://leetcode.com/problems/array-partition

---
+++ array-partition.ts
@@ -0,0 +1,13 @@
+{
+    const arrayPairSum = function(nums) {
+        nums.sort((a,b) => a - b)
+        
+        let result = 0;
+
+        for(let i = 0; i < nums.length; i += 2) {
+            result += nums[i]
+        }
+
+        return result
+    };
+}
