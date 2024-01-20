241b0c695fb31d2d536d6a8c71ca6bac7049c955
traximullin <traximullin@bk.ru>
2024-01-20 18:28:24 +0300

    https://leetcode.com/problems/sum-of-subarray-minimums

--- sum-of-subarray-minimums.ts
+++ sum-of-subarray-minimums.ts
@@ -2,6 +2,7 @@
     var sumSubarrayMins = function (a) {
         const MOD = 1e9 + 7;
         let sum = 0;
+        
         for (let i = 0; i < a.length; i++) {
           let small = a[i];
           for (let j = i; j < a.length; j++) {
@@ -11,5 +12,5 @@
         }
       
         return sum % MOD;
-      };
+    };
 }
