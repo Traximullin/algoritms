46467850b83e5c8550cd4ad6747bd90b53d8e1b6
traximullin <traximullin@bk.ru>
2024-02-10 14:01:43 +0300

    https://www.codewars.com/kata/520d9c27e9940532eb00018e

---
+++ 520d9c27e9940532eb00018e.ts
@@ -0,0 +1,20 @@
+{
+    function solution(...args){
+        const s = new Set();
+
+        for(const arg of args) {
+            if(s.has(arg)) {
+                return true
+            }
+
+            s.add(arg)
+        }
+
+        return false
+    }
+
+
+    console.log(solution('a','b','c','a'))
+    console.log(solution(1,2,3));
+    console.log(solution(solution(1,2,3,'a','b')))
+}
