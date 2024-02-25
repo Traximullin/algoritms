373eb9191c76b7b89308364adc1921acaede6af3
traximullin <traximullin@bk.ru>
2024-02-25 13:14:05 +0300

    https://leetcode.com/problems/strictly-palindromic-number

---
+++ strictly-palindromic-number.ts
@@ -0,0 +1,35 @@
+{
+    function isPalindromic(binary: string) {
+        let start = 0,
+            end = binary.length - 1;
+
+        while(start < end) {
+            if(binary[start] !== binary[end]) {
+                return false
+            }
+
+            start++
+            end--
+
+        }
+        
+        return true
+
+    }
+
+    function isStrictlyPalindromic(n: number): boolean {
+        const maxPal = n - 2;
+
+        for(let i = 2; i <= maxPal; i++) {
+            const b = n.toString(i)
+
+            if(!isPalindromic(b)) 
+                return false
+    
+        }
+
+        return true
+    };
+
+    console.log(isStrictlyPalindromic(9))
+}
