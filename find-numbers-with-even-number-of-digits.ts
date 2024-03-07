17b4fdd128472a5a2baa248c4a0860342010dd55
traximullin <traximullin@bk.ru>
2024-03-07 21:20:18 +0300

    https://leetcode.com/problems/find-numbers-with-even-number-of-digits

--- find-numbers-with-even-number-of-digits.ts
+++ find-numbers-with-even-number-of-digits.ts
@@ -3,21 +3,15 @@
         let c = 0;
 
         for(let num of nums) {
-            let num_length = 0;
 
-            while(num > 0) {
-                num =  num / 10
-                num_length++
-            }
-
-            if(num_length % 2 == 0) {
+            if(String(num).length % 2 == 0) {
                 c++
             }
-
+            
         }
 
         return c;
     };
 
-    console.log(findNumbers([12,345,2,6,7896]))
+    console.log(findNumbers([555,901,482,1771]))
 }
