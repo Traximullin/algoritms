c69b93738c3e7e47df78b1dcf294366339ee2079
traximullin <traximullin@bk.ru>
2024-03-25 20:30:08 +0300

    https://leetcode.com/problems/find-all-duplicates-in-an-array

---
+++ find-all-duplicates-in-an-array.ts
@@ -0,0 +1,16 @@
+{
+    function findDuplicates(nums: number[]): number[] {
+        const hash = new Set();
+        
+        return nums.filter(num => {
+            if(hash.has(num)) {
+                return true
+            } else {
+                hash.add(num)
+            }
+        })
+
+    };
+
+    console.log(findDuplicates([4,3,2,7,8,2,3,1]))
+}
