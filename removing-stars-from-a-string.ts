f33eedbb6664eeebce790539e64a01b81805f6bf
traximullin <traximullin@bk.ru>
2024-05-31 16:42:35 +0300

    https://leetcode.com/problems/removing-stars-from-a-string

---
+++ removing-stars-from-a-string.ts
@@ -0,0 +1,17 @@
+{
+    function removeStars(s: string): string {
+        const stack: string[] = [];
+
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] === '*') {
+                stack.pop();
+            } else {
+                stack.push(s[i])
+            }
+        }
+
+        return stack.join("")
+    };
+    
+    console.log(removeStars("leet**cod*e"))
+}
