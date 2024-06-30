e04ca19c60655175b25165efaac217d9cf0570de
traximullin <traximullin@bk.ru>
2024-06-30 23:32:47 +0300

    https://leetcode.com/problems/remove-all-occurrences-of-a-substring

---
+++ remove-all-occurrences-of-a-substring.ts
@@ -0,0 +1,11 @@
+{
+    function removeOccurrences(s: string, part: string): string {
+        while (s.includes(part)) {
+            s = s.replace(part, "")
+        }
+
+        return s
+    };
+
+    console.log(removeOccurrences("daabcbaabcbc", 'abc'))
+}
