329c81e7c01fe403712b92bc7c9e627b999cb05f
traximullin <traximullin@bk.ru>
2024-08-05 15:50:50 +0300

    https://leetcode.com/problems/account-balance-after-rounded-purchase/

---
+++ account-balance-after-rounded-purchase.ts
@@ -0,0 +1,7 @@
+{
+    function accountBalanceAfterPurchase(purchaseAmount: number): number {
+        return 100 - Math.round(purchaseAmount / 10) * 10
+    };
+
+    console.log(accountBalanceAfterPurchase(9))
+}
