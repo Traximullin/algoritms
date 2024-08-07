d9b465cbab1f8fe001550a9df56c8572b730a43d
traximullin <traximullin@bk.ru>
2024-08-07 17:03:10 +0300

    https://leetcode.com/problems/adding-spaces-to-a-string/

---
+++ adding-spaces-to-a-string.ts
@@ -0,0 +1,20 @@
+{
+    function addSpaces(s: string, spaces: number[]): string {
+        let result = "";
+        let current_spaces = 0;
+
+        for (let i = 0; i < s.length; i++) {
+            if (spaces[current_spaces] === i) {
+                result += ` ${s[i]}`
+                current_spaces += 1;
+            } else {
+                result += s[i]
+            }
+        }
+
+        return result
+    };
+
+    console.log(addSpaces('LeetcodeHelpsMeLearn', [8,13,15]))
+
+}
