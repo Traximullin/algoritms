cc023deda708b5384cc30e02f9ab8e43af896cfb
traximullin <traximullin@bk.ru>
2024-10-17 18:46:33 +0300

    https://leetcode.com/problems/remove-letter-to-equalize-frequency

--- remove-letter-to-equalize-frequency.ts
+++ remove-letter-to-equalize-frequency.ts
@@ -1,29 +1,32 @@
 {
+    function allEqualOrZero(array: number[]) {
+        const firstNonZero = array.find(item => item !== 0);
+
+        return array.every(item => item === 0 || item === firstNonZero);
+    }
+
     function equalFrequency(word: string): boolean {
-        const array = Array.from<number>({length: 26}).fill(0)
-    
+        let array = new Array<number>(26).fill(0);
+
         for (const char of word) {
-            const key = char.charCodeAt(0) - 97;
-    
-            array[key] += 1;
+            array[char.charCodeAt(0) - 97] += 1;
         }
-    
-        const min = Math.min(...array);
-    
-        if (array.every(item => item === min)) 
-            return true;
-    
-        let c = 0;
-    
-        array.forEach(item => {
-            if (item !== min) {
-                console.log(item) 
-                c += item - min
-            }
-        })
-        console.log(c)
-        return c === 1;
+
+        array = array.filter(Number);
+
+        for (let i = 0; i < array.length; i++) {
+            const copy_array = new Array(...array);
+
+            copy_array[i] = array[i] - 1;
+
+            const flag = allEqualOrZero(copy_array);
+
+            if (flag)
+                return true
+        }
+
+        return false;
     };
 
-    console.log(equalFrequency("abcc"))
+    console.log(equalFrequency("bac"))
 }
