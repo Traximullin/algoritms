239488481b36c5fb01b1b53c8dd03aaf9e005ca3
traximullin <traximullin@bk.ru>
2024-11-19 18:25:55 +0300

    https://leetcode.com/problems/find-the-width-of-columns-of-a-grid

--- find-the-width-of-columns-of-a-grid.ts
+++ find-the-width-of-columns-of-a-grid.ts
@@ -1,7 +1,16 @@
 {
-    // TODO
     function findColumnWidth(grid: number[][]): number[] {
-        const result: number[] = []
+        const result = new Array<number>(grid[0].length).fill(0);
+
+        for(let i = 0; i < grid.length; i++) {
+
+            for(let j = 0; j < grid[i].length; j++) {
+                const current_length = String(grid[i][j]).length;
+                
+                if(result[j] < current_length) 
+                    result[j] = current_length
+            }
+        }
 
         return result;
     };
