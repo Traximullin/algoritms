e35f44bf26eff86af3bc7e114685075a6b52c1ec
traximullin <traximullin@bk.ru>
2024-12-08 14:56:27 +0300

    https://leetcode.com/problems/remove-stones-to-minimize-the-total

--- remove-stones-to-minimize-the-total.ts
+++ remove-stones-to-minimize-the-total.ts
@@ -1,20 +1,31 @@
 {
+    class MaxPriorityQueue {
+        enqueue() {}
+    }
+
     function minStoneSum(piles: number[], k: number): number {
-        while(k) {
-            let max_index = 0;
-
-            for (let i = 0; i < piles.length; i++) 
-                if (piles[max_index] < piles[i]) 
-                    max_index = i;
-
-            piles[max_index] = Math.ceil(piles[max_index] / 2);
-
-            k--;
+        const queue = new MaxPriorityQueue();
+        let c = 0;
+    
+        for (let i = 0, n = piles.length; i < n; i++) {
+            queue.enqueue(piles[i]);
+            c += piles[i];
         }
+    
+        while (k--) {
+            let { element } = queue.dequeue();
 
-        return piles.reduce((prev, curr) => prev + curr, 0)
+            c -= element;
+            element -= Math.floor(element / 2);
+            c += element;
+            
+            queue.enqueue(element);
+        }
+    
+        return c;
+    
     };
 
-    console.log(minStoneSum([4122,9928,3477,9942], 6))
+    console.log(minStoneSum([5,4,9], 2))
     // console.log(minStoneSum([4,3,6,7],3))
 }
