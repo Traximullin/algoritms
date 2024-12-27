15951c4d577c1603aa1e8dc4c094ccea016094de
traximullin <traximullin@bk.ru>
2024-12-27 23:07:14 +0300

    https://leetcode.com/problems/best-sightseeing-pair

---
+++ best-sightseeing-pair.ts
@@ -0,0 +1,21 @@
+{
+    function maxScoreSightseeingPair(values: number[]): number {
+        let max = 0,
+            score = values[0];
+
+        for (let i = 1; i < values.length; i++) {
+            --score;
+
+            if (score + values[i] > max)
+                max = score + values[i];
+
+            if (values[i] > score)
+                    score = values[i]
+        }
+
+        return max;
+    };
+
+
+    console.log(maxScoreSightseeingPair([8,1,5,2,6]))
+}
