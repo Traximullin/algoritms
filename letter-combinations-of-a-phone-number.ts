3da8428b80b845cf44154bbe8615d442317450c0
traximullin <traximullin@bk.ru>
2025-01-04 15:11:29 +0300

    https://leetcode.com/problems/set-matrix-zeroes

--- letter-combinations-of-a-phone-number.ts
+++ letter-combinations-of-a-phone-number.ts
@@ -1,5 +1,5 @@
 {
-    const keyboards = {
+    const keyboards: Record<string, string> = {
         "2": "abc",
         "3": "def",
         "4": "ghi",
@@ -9,5 +9,21 @@
         "8": "tuv",
         "9": "wxyz"
     }
+
+    function letterCombinations(digits: string): string[] {
+        const result = Array(keyboards[digits[0]].length).fill('');
+
+        for (const digit of digits) {
+            console.log(digit)
+
+            for (const [index, key_one] of keyboards[digit].split('').entries()) {
+                result[index] += key_one;
+            }
+        }
+
+        console.log(result)
+    };
+
+    console.log(letterCombinations('23'))
 
 }
