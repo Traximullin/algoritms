d94f909adadc500ea10f0dd6aa0abfa2297becd3
traximullin <traximullin@bk.ru>
2025-01-12 17:11:18 +0300

    https://leetcode.com/problems/longest-consecutive-sequence

--- minimum-size-subarray-sum.ts
+++ minimum-size-subarray-sum.ts
@@ -1,22 +1,28 @@
 {
-    // !TODO 
-    // https://leetcode.com/problems/minimum-size-subarray-sum/?envType=study-plan-v2&envId=top-interview-150
-    const minSubArrayLen = function(target, nums) {
-        nums.sort()
+    function minSubArrayLen(target: number, nums: number[]): number {
+        const length = nums.length;
+        nums.sort((a,b) => a - b);
+        let c = 0;
 
-        let result = 0;
-        let count = 0;
-        for(let i = nums.length - 1; i > 0; i--) {
-            if(count !== target) {
-                count += nums[i]
-                result++
+        while (nums.length) {
+            console.log(c)
+            const sum = c + nums.at(-1);
+            console.log(sum);
+            if (sum === target) 
+                return length - nums.length + 1;
+
+            if (sum > target) {
+                c += nums.shift()!;
             } else {
-                break
+                c += nums.pop()!;
             }
         }
 
-        return result
+        return 0;
     };
 
-    console.log(minSubArrayLen(7, [2,3,1,2,4,3]))
+    // console.log(minSubArrayLen(7, [2,3,1,2,4,3]))
+    // console.log(minSubArrayLen(4, [1,4,4]))
+    console.log(minSubArrayLen(11, [1,2,3,4,5]))
+    // console.log(minSubArrayLen(11,  [1,1,1,1,1,1,1,1]))
 }
