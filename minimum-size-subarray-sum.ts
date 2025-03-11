82e528bd736d63fdf6ab83813d12791f50258400
traximullin <traximullin@bk.ru>
2025-03-11 21:11:17 +0300

    https://leetcode.com/problems/minimum-size-subarray-sum

--- minimum-size-subarray-sum.ts
+++ minimum-size-subarray-sum.ts
@@ -1,28 +1,25 @@
 {
     function minSubArrayLen(target: number, nums: number[]): number {
-        const length = nums.length;
-        nums.sort((a,b) => a - b);
-        let c = 0;
+        let min_len = Infinity;
+        let sum = 0;
+        let start = 0;
 
-        while (nums.length) {
-            console.log(c)
-            const sum = c + nums.at(-1);
-            console.log(sum);
-            if (sum === target) 
-                return length - nums.length + 1;
+        for (let i = 0; i < nums.length; i++) {
+            sum += nums[i];
 
-            if (sum > target) {
-                c += nums.shift()!;
-            } else {
-                c += nums.pop()!;
+            while (sum >= target) {
+                min_len = Math.min(min_len, i - start + 1);
+                sum -= nums[start];
+                start += 1;
             }
+
         }
 
-        return 0;
+        return min_len === Infinity ? 0 : min_len;
     };
 
-    // console.log(minSubArrayLen(7, [2,3,1,2,4,3]))
-    // console.log(minSubArrayLen(4, [1,4,4]))
+    console.log(minSubArrayLen(7, [2,3,1,2,4,3]))
+    console.log(minSubArrayLen(4, [1,4,4]))
     console.log(minSubArrayLen(11, [1,2,3,4,5]))
-    // console.log(minSubArrayLen(11,  [1,1,1,1,1,1,1,1]))
+    console.log(minSubArrayLen(11,  [1,1,1,1,1,1,1,1]))
 }
