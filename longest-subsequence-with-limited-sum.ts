f9fd9903b8c4b63b9104e6dc94d0681a83514fa9
traximullin <traximullin@bk.ru>
2025-03-16 21:25:33 +0300

    https://leetcode.com/problems/longest-subsequence-with-limited-sum

--- longest-subsequence-with-limited-sum.ts
+++ longest-subsequence-with-limited-sum.ts
@@ -1,6 +1,26 @@
 {
-    // todo
     function answerQueries(nums: number[], queries: number[]): number[] {
-    
+        nums.sort((a, b) => a - b);
+        
+        const res: number[] = [];
+
+        for (let query of queries) {
+            let sum= 0, 
+                num= 0;
+            
+            for (let i = 0; i < nums.length; i++) {
+                const prefix = sum + nums[i];
+                
+                if (prefix > query)
+                    break
+
+                sum += nums[i]
+                num++
+            }
+
+            res.push(num);
+        }
+            
+        return res;   
     };
 }
