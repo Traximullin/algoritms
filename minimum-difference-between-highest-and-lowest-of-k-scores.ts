61fc4d5cba63b122fab121d3fd6ea5709af17405
traximullin <traximullin@bk.ru>
2025-03-20 18:35:01 +0300

    minimum-difference-between-highest-and-lowest-of-k-scores

---
+++ minimum-difference-between-highest-and-lowest-of-k-scores.ts
@@ -0,0 +1,17 @@
+{
+    function minimumDifference(nums: number[], k: number): number {
+        if (k == 1)
+            return 0;
+
+        nums.sort((a, b) => a - b);
+
+        let min = Infinity;
+
+        for (let i = 0; i <= nums.length - k; i++)
+            min = Math.min(min, nums[i + k - 1] - nums[i]);
+
+        return min;
+    };
+    
+    console.log(minimumDifference([9,4,1,7], 2))
+}
