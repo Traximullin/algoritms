f8aec2bf93cc1c0b1d62077fe1181d953872100b
traximullin <traximullin@bk.ru>
2025-03-27 19:27:38 +0300

    https://leetcode.com/problems/decode-string

--- decode-string.ts
+++ decode-string.ts
@@ -1,47 +1,46 @@
 {
-    // todo 
-    // https://leetcode.com/problems/decode-string/
     function decodeString(s: string): string {
-        const stack: number[] = [];
+        const stack = [];
+        const nums = [];
+    
         let num = "";
-        const stack_nums: string[] = [];
-        let count = 0;
-
+        let test = "";
+    
         for (let i = 0; i < s.length; i++) {
             const char = s[i];
-            count += 1;
-
-            if (count > 40) {
-                break
-            }
-            if (!Number.isNaN(Number(char))) {
+    
+            if (!isNaN(Number(char))) { 
                 num += char;
+                continue;
             }
-
+    
             if (char === "[") {
-                stack.push(i + 1);
-                stack_nums.push(num);
+                nums.push(Number(num)); 
                 num = "";
+                stack.push(test);
+                test = "";
+                continue;
             }
     
             if (char === "]") {
-                const open_index = stack.pop() || 1;
-                const delete_number = stack_nums.pop();
-                const count_repeats = Number(delete_number) || 0;
-
-                const value = s.slice(open_index, i).repeat(count_repeats);
-                console.log(value)
-                console.log(s.slice(open_index - (delete_number?.length || 1) - 1, i + 1))
-                s = s.replace(s.slice(open_index - (delete_number?.length || 1) - 1, i + 1), value)
-
-                i -= count_repeats;
+                const element = test; 
+                const repeats = nums.pop()!; 
+    
+                test = stack.pop()! + element.repeat(repeats); 
+                continue;
             }
     
+            test += char;
         }
     
-        return s
-    };
-    console.log([1,2,3].slice(1,3))
-    // console.log(decodeString("3[a2[c]]"))
-    console.log(decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef"))
+        return test; 
+    }
+    
+    console.log(decodeString('3[z]2[2[y]pq4[2[jk]e1[f]]]ef'));
+    
+    
+    
+    console.log(decodeString("leetcode"))
+    console.log(decodeString("3[a2[c2[de]]]"))
+    console.log(decodeString("2[abc]3[cd]ef"))
 }
