97355c3f2050e31cb80b79a18a18fcf0eee69d4f
traximullin <traximullin@bk.ru>
2025-04-10 18:17:35 +0300

    https://leetcode.com/problems/maximum-containers-on-a-ship/

--- maximum-containers-on-a-ship.ts
+++ maximum-containers-on-a-ship.ts
@@ -2,4 +2,6 @@
     function maxContainers(n: number, w: number, maxWeight: number): number {
         return Math.min(n ** 2, maxWeight / w | 0);
     };
+
+    console.log(maxContainers(2,3, 15))
 }
