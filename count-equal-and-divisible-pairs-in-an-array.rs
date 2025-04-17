2f11ad0b039a544d82abaa2a9d84b175c4c32169
traximullin <traximullin@bk.ru>
2025-04-17 18:13:48 +0300

    https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/

---
+++ count-equal-and-divisible-pairs-in-an-array.rs
@@ -0,0 +1,13 @@
+pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
+    let mut c = 0;
+
+    for i in 0..nums.len() {
+        for j in i + 1..nums.len() {
+            if nums[i] == nums[j] && (i * j) as i32 % k == 0 {
+                c += 1;
+            }
+        }
+    }
+
+    return c;
+}
