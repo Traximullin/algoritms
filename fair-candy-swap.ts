962438b43f2321cf283cc8ab36736c4d80760217
Тимур <traximullin@bk.ru>
2025-05-10 19:24:29 +0300

    https://leetcode.com/problems/fair-candy-swap/

--- fair-candy-swap.ts
+++ fair-candy-swap.ts
@@ -1,9 +1,21 @@
 {
  
     function fairCandySwap(aliceSizes: number[], bobSizes: number[]): number[] {
-        const alise = aliceSizes.reduce((prev, next) => prev += next, 0);
-        const bob = bobSizes.reduce((prev, next) => prev += next, 0);
+        const sumA = aliceSizes.reduce((acc, x) => acc + x, 0);
+        const sumB = bobSizes.reduce((acc, x) => acc + x, 0);
+        const delta = (sumA - sumB) / 2;
         
+        const setB = new Set(bobSizes);
+        
+        for (let x of aliceSizes) {
+            
+            const y = x - delta;
+            if (setB.has(y)) {
+                return [x, y];
+            }
+        }
+
+        return [0, 0];
     };
 
     console.log(fairCandySwap([1,1], [2,2]))
