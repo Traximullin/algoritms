78a26618ec77e86f5f66319deb7c56b89489388a
Тимур <traximullin@bk.ru>
2025-06-17 18:18:32 +0300

    https://leetcode.com/problems/non-overlapping-intervals/

--- non-overlapping-intervals.ts
+++ non-overlapping-intervals.ts
@@ -1,20 +1,20 @@
 {
     function eraseOverlapIntervals(intervals: number[][]): number {
-    let res = 0;
-    intervals.sort((x, y)=>x[1] - y[1]);
-    let lastEnd = -Infinity;
+        let res = 0;
+        intervals.sort((x, y)=>x[1] - y[1]);
+        let lastEnd = -Infinity;
 
-    for(const interval of intervals){
-        const start = interval[0];
-        const end = interval[1];
+        for(const interval of intervals){
+            const start = interval[0];
+            const end = interval[1];
 
-        if(start>=lastEnd){
-            lastEnd = end;
-        }else{
-            res += 1;
+            if(start>=lastEnd){
+                lastEnd = end;
+            }else{
+                res += 1;
+            }
         }
-    }
 
-    return res;
-};
+        return res;
+    };
 }
