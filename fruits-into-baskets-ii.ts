3e4c618431fb7f4e19fcd69be0b1fd94cf22999c
Тимур <traximullin@bk.ru>
2025-08-06 12:00:24 +0300

    https://leetcode.com/problems/teemo-attacking/

--- fruits-into-baskets-ii.ts
+++ fruits-into-baskets-ii.ts
@@ -1,14 +1,14 @@
 {
     function numOfUnplacedFruits(fruits: number[], baskets: number[]): number {
-    let unplaced = fruits.length;
-    for (const fruit of fruits) {
-        const foundIndex = baskets.findIndex(b => b >= fruit);
-        if (foundIndex > -1) {
-            baskets[foundIndex] = -1;
-            unplaced--;
+        let unplaced = fruits.length;
+        for (const fruit of fruits) {
+            const foundIndex = baskets.findIndex(b => b >= fruit);
+            if (foundIndex > -1) {
+                baskets[foundIndex] = -1;
+                unplaced--;
+            }
         }
-    }
 
-    return unplaced;
-};
+        return unplaced;
+    };
 }
