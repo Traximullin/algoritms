10c9f4d351446ca381dd9e3b112e73efd6c2ff8f
Тимур <traximullin@bk.ru>
2025-11-10 22:16:26 +0300

    https://leetcode.com/problems/minimum-operations-to-convert-all-elements-to-zero/

---
+++ minimum-operations-to-convert-all-elements-to-zero.ts
@@ -0,0 +1,23 @@
+{
+    const minOperations = (nums: number[]): number => {
+        const activeStack: number[] = [];
+        let operationCount = 0;
+
+        for (const currentValue of nums) {
+            while (activeStack.length > 0 && activeStack[activeStack.length - 1] > currentValue) {
+                activeStack.pop();
+            }
+
+            if (currentValue === 0) {
+                continue;
+            }
+
+            if (activeStack.length === 0 || activeStack[activeStack.length - 1] < currentValue) {
+                operationCount++;
+                activeStack.push(currentValue);
+            }
+        }
+
+        return operationCount;
+    };
+}
