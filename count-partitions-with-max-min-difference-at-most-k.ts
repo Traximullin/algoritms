a88938bcebd3f396d40af251823eaaba1009beb7
Тимур <traximullin@bk.ru>
2025-12-06 16:23:17 +0300

    https://leetcode.com/problems/count-partitions-with-max-min-difference-at-most-k

---
+++ count-partitions-with-max-min-difference-at-most-k.ts
@@ -0,0 +1,28 @@
+function countPartitions(nums: number[], k: number): number {
+    const n = nums.length, MOD = 1_000_000_007;
+    const dp: number[] = Array(n + 1).fill(0);
+    dp[0] = 1;
+
+    const mx: number[] = [];
+    const mn: number[] = [];
+    let l = 0, sum = 0;
+
+    for (let r = 0; r < n; r++) {
+        while (mx.length && nums[mx[mx.length - 1]] <= nums[r]) mx.pop();
+        while (mn.length && nums[mn[mn.length - 1]] >= nums[r]) mn.pop();
+        mx.push(r);
+        mn.push(r);
+
+        while (nums[mx[0]] - nums[mn[0]] > k) {
+            if (mx[0] === l) mx.shift();
+            if (mn[0] === l) mn.shift();
+            sum = (sum - dp[l] + MOD) % MOD;
+            l++;
+        }
+
+        sum = (sum + dp[r]) % MOD;
+        dp[r + 1] = sum;
+    }
+
+    return dp[n];
+}
