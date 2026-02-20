f929528a56d78cc46cd9bfc038146dfecf020662
Тимур <traximullin@bk.ru>
2026-02-20 17:55:06 +0300

    https://leetcode.com/problems/valid-sudoku/submissions/1925463965/

--- valid-sudoku.ts
+++ valid-sudoku.ts
@@ -1,34 +1,17 @@
 function isValidSudoku(board: string[][]): boolean {
-  const seenRows = [...Array(9)].map(() => new Set<string>());
-  const seenCols = [...Array(9)].map(() => new Set<string>());
-  const seenBoxs = [...Array(9)].map(() => new Set<string>());
+  const set = new Set();
 
-  for (let r = 0; r < 9; r++) {
-    for (let c = 0; c < 9; c++) {
-      const item = board[r][c];
-      if (item === ".") continue;
-      const boxN = getBox(r, c);
-
-      if (
-        seenRows[r].has(item) ||
-        seenCols[c].has(item) ||
-        seenBoxs[boxN].has(item)
-      ) {
-        return false;
-      }
-
-      seenRows[r].add(item);
-      seenCols[c].add(item);
-      seenBoxs[boxN].add(item);
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const cell = board[i][j];
+      if (cell === ".") continue;
+      const row = `row: ${i}, value: ${cell}`;
+      const column = `column: ${j}, value: ${cell}`;
+      const boxNumber = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+      const box = `boxNumber: ${boxNumber}, value: ${cell}`;
+      if (set.has(row) || set.has(column) || set.has(box)) return false;
+      set.add(row).add(column).add(box);
     }
   }
-
   return true;
-}
-
-function getBox(r: number, c: number): number {
-  const rowBoxIndex = Math.floor(r / 3);
-  const colBoxIndex = Math.floor(c / 3);
-
-  return rowBoxIndex * 3 + colBoxIndex;
 }
