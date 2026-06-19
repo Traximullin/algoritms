480a54740f5a12e70414261b95e1fb44ba0bd692
Тимур <traximullin@bk.ru>
2026-06-19 16:38:31 +0300

    https://leetcode.com/problems/find-the-highest-altitude/?envType=daily-question&envId=2026-06-19

---
+++ find-the-highest-altitude.rs
@@ -0,0 +1,6 @@
+pub fn largest_altitude(gain: Vec<i32>) -> i32 {
+    gain.into_iter()
+        .chain(std::iter::once(0))
+        .scan(0, |c, g| Some(std::mem::replace(c, *c + g)))
+        .fold(0, Ord::max)
+}
