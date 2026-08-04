14dc95295028097663390071178732462a69d2ea
Тимур <traximullin@bk.ru>
2026-08-04 23:01:04 +0300

    https://leetcode.com/problems/find-missing-elements/?envType=daily-question&envId=2026-08-04

---
+++ find-missing-elements.ts
@@ -0,0 +1,14 @@
+function findMissingElements(nums: number[]): number[] {
+    nums.sort((a, b) => a - b);
+
+    const missing = [];
+    for (let i = 0, curNum = nums[0]; i < nums.length; i++) {
+        while (curNum !== nums[i]) {
+            missing.push(curNum++);
+        }
+
+        curNum++
+    }
+
+    return missing;
+};
