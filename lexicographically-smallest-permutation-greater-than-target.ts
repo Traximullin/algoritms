2e7b25e7ec7ecc4b4eccd079d697edda65d0471a
Тимур <traximullin@bk.ru>
2026-08-27 23:11:35 +0300

    https://leetcode.com/problems/lexicographically-smallest-permutation-greater-than-target/?envType=daily-question&envId=2026-08-27

---
+++ lexicographically-smallest-permutation-greater-than-target.ts
@@ -0,0 +1,64 @@
+function lexGreaterPermutation(s: string, target: string): string {
+    const n = s.length;
+
+    const cnt: number[] = new Array(26).fill(0);
+
+    for (const ch of s) {
+        cnt[ch.charCodeAt(0) - 97]++;
+    }
+
+    let p = 0;
+
+    while (p < n) {
+        const c = target.charCodeAt(p) - 97;
+
+        if (cnt[c] === 0) {
+            break;
+        }
+
+        cnt[c]--;
+        p++;
+    }
+
+    let i = p;
+
+    while (i >= 0) {
+        if (i < n) {
+            const t = target.charCodeAt(i) - 97;
+            let pick = -1;
+
+            for (let c = t + 1; c < 26; c++) {
+                if (cnt[c] > 0) {
+                    pick = c;
+                    break;
+                }
+            }
+
+            if (pick >= 0) {
+                cnt[pick]--;
+
+                let tail = "";
+
+                for (let c = 0; c < 26; c++) {
+                    tail += String
+                        .fromCharCode(97 + c)
+                        .repeat(cnt[c]);
+                }
+
+                cnt[pick]++;
+
+                return target.slice(0, i)
+                    + String.fromCharCode(97 + pick)
+                    + tail;
+            }
+        }
+
+        i--;
+
+        if (i >= 0) {
+            cnt[target.charCodeAt(i) - 97]++;
+        }
+    }
+
+    return "";
+};
