ade435893752f079a08dec03c4dc8025255563af
Тимур <traximullin@bk.ru>
2026-08-30 22:13:48 +0300

    https://leetcode.com/problems/removing-minimum-and-maximum-from-array

---
+++ removing-minimum-and-maximum-from-array.ts
@@ -0,0 +1,22 @@
+function minimumDeletions(nums: number[]): number {
+    const n = nums.length;
+    let min = 0, max = 0;
+    let minVal = Infinity, maxVal = -Infinity;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] < minVal) {
+            min = i;
+            minVal = nums[i];
+        }
+        if (nums[i] > maxVal) {
+            max = i;
+            maxVal = nums[i];
+        }
+    }
+
+    return Math.min(
+        Math.min(min, max) + 1 + (n - Math.max(min, max)),
+        n - Math.min(min, max),
+        Math.max(min, max) + 1
+    );
+};
